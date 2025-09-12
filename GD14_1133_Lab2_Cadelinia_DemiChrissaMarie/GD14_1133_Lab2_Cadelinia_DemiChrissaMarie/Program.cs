7a89347ddae0c6bba5a2b3337e4776c0aed9b640
kizu-01 <demicadelinia.personal@gmail.com>
2025-09-11 20:58:48 -0700

    Lab 2 - Learning to Count
    Deleted the extra sln file and did the changes in Hexadecimal and blank spaces

--- GD14_1133_Lab2_Cadelinia_DemiChrissaMarie/GD14_1133_Lab2_Cadelinia_DemiChrissaMarie/Program.cs
+++ GD14_1133_Lab2_Cadelinia_DemiChrissaMarie/GD14_1133_Lab2_Cadelinia_DemiChrissaMarie/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello! I am Demi Chrissa Marie Cadelinia and this is my 2nd Assignment Task as of September 11, 2025.");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("Here are the following numbers in all three bases:");
-            Console.WriteLine("");
+            Console.WriteLine();
             Console.WriteLine("Decimal: 0     »   Binary: 00000000   »   Hex: 0");
             Console.WriteLine("Decimal: 1     »   Binary: 00000001   »   Hex: 1");
             Console.WriteLine("Decimal: 2     »   Binary: 00000010   »   Hex: 2");
@@ -17,8 +17,8 @@
             Console.WriteLine("Decimal: 32    »   Binary: 00100000   »   Hex: 20");
             Console.WriteLine("Decimal: 64    »   Binary: 01000000   »   Hex: 40");
             Console.WriteLine("Decimal: 100   »   Binary: 01100100   »   Hex: 64");
-            Console.WriteLine("Decimal: 255   »   Binary: 11111111   »   Hex: ff");
-            Console.WriteLine("");
+            Console.WriteLine("Decimal: 255   »   Binary: 11111111   »   Hex: FF");
+            Console.WriteLine();
             Console.WriteLine("Done on September 11, 2025. Goodbye, and thank you for checking my simple code assignment!");
         }
     }
