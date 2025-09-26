ea8e58ff85e8903984f15ba42dce5a9be48bb263
kizu-01 <demicadelinia.personal@gmail.com>
2025-09-25 18:09:57 -0700

    Lab 4 - Throwing Dice (One Round)
    - Added playerName to request player's name
    - Changed DieRoller script name to Die as per requirements
    - Changed date details
    - Added if else statements for asking player to play with bool function containing 3 different outcomes
    - Added hard text instructions for the game
    - Added decide turn order using bool function with if else statements for player and opponent
    - Added string with if else statements and other variables such as maxSides and allowedDice to balance the die choice of the player and computer
    - Added if else statements for end results of the game
    - Added score of the results for player and opponent

--- GD14_1133_Lab3_DiceGame_Cadelinia_Demi/GD14_1133_Lab3_DiceGame_Cadelinia_Demi/Scripts/GameManager.cs
+++ GD14_1133_Lab3_DiceGame_Cadelinia_Demi/GD14_1133_Lab3_DiceGame_Cadelinia_Demi/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,97 +9,161 @@ namespace GD14_1133_Lab3_DiceGame_Cadelinia_Demi.Scripts
 {
     internal class GameManager
     {
+        private Player player;
+        private Player computer;
+        private Die dice = new Die();
+        private Random rd = new Random();
         public void Play()
         {
-            Intro();
+            if (!Intro())
+                return;
+
+            Instructions();
             Game();
-            OperatorExamples();
             Outro();
         }
-        
         // Show welcome message
-        private void Intro()
+        private bool Intro()
         {
             Console.WriteLine("∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘");
-            Console.WriteLine("Welcome to The Dice Game! This is made by Demi Cadelinia as of September 18, 2025.");
+            Console.WriteLine("Welcome to The Dice Game! Made by Demi Cadelinia as of September 25, 2025.");
             Console.WriteLine("∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘");
             Console.WriteLine();
 
+            Console.WriteLine("Please enter your name: ");
+            string playerName = Console.ReadLine();
+            player = new Player(playerName);
+            computer = new Player("Opponent");
+            Console.WriteLine();
+
+            Console.WriteLine("Do you want to play? [Yes/No]");
+            string answer = Console.ReadLine().ToLower();
+            Console.WriteLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("Great, Let's Play!");
+                return true;
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Thanks for your time. Goodbye!");
+                return false;
+            }
+            else
+            {
+                Console.WriteLine("That's alright! You don't have to play. Exiting game...");
+                return false;
+            }
         }
-        
+        // Show game instructions
+        private void Instructions()
+        {
+            Console.WriteLine();
+            Console.WriteLine("How to Play:");
+            Console.WriteLine("1. You and your opponent will each roll a dice.");
+            Console.WriteLine("2. You get to choose which die to roll (d4, d6, d8, d12, or d20).");
+            Console.WriteLine("3. The opponent will automatically choose a die.");
+            Console.WriteLine("4. Whoever rolls the higher number wins the round and gets a point.");
+            Console.WriteLine("5. If it’s a tie, no one gets a point and the game ends.");
+        }
         // Dice rolls for 4 multiple dice
         private void Game()
-        {     
-            DieRoller dice = new DieRoller();
-
-            Console.WriteLine("Let's Roll!");
-            Console.WriteLine();
-
-            int total = dice.RollAll();
-
-            // Add the overall sum of rolls
-            Console.WriteLine($"Total Score: {total}");
-            Console.WriteLine();
-            Console.WriteLine("∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘-∘");
-            Console.WriteLine();
-        }
-        
-        // Explain arithmetic operators with examples
-        private void OperatorExamples()
         {
-            // Generate random numbers for operator examples
-            Random rd = new Random();
+            Console.WriteLine();
+            Console.WriteLine("That's it. Let the game begin!");
+            Console.WriteLine();
+            Console.WriteLine("- Press any key to decide turn order -");
+            Console.WriteLine();
+            Console.ReadKey();
 
-            int a = rd.Next(1, 11);
-            int b = rd.Next(1, 11);
-            
-            int c = a;  // For increment example value
-            int d = b;  // For decrement example value
-            float e = a;    // For division to get decimal answers
-            float f = b;
+            // Decide turn order
+            bool playerFirst = rd.Next(0, 2) == 0;
 
-            Console.WriteLine("Arithmetic Operators in C#");
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+            int playerScore, computerScore;
+            int playerChosenSides = 0, computerChosenSides = 0;
+
+            if (playerFirst)
+            {
+                Console.WriteLine($"{player.Name} goes first!");
+                playerScore = PlayerTurn(20, out playerChosenSides);
+                computerScore = ComputerTurn(playerChosenSides, out computerChosenSides);
+            }
+            else
+            {
+                Console.WriteLine("Opponent goes first!");
+                computerScore = ComputerTurn(20, out computerChosenSides);
+                playerScore = PlayerTurn(computerChosenSides, out playerChosenSides);
+            }
+
             Console.WriteLine();
 
-            // Addition
-            Console.WriteLine("+ (Addition): adds the value of numbers");
-            Console.WriteLine($"Example: {a} + {b} = {a + b}");
+            if (playerScore > computerScore)
+            {
+                Console.WriteLine($"{player.Name} Wins!");
+                player.Score++;
+            }
+            else if (computerScore > playerScore)
+            {
+                Console.WriteLine($"{computer.Name} Wins!");
+                computer.Score++;
+            }
+            else
+            {
+                Console.WriteLine("It's a tie!");
+            }
             Console.WriteLine();
-
-            // Subtraction
-            Console.WriteLine("- (Subtraction): deducts the value of numbers");
-            Console.WriteLine($"Example: {a} - {b} = {a - b}");
-            Console.WriteLine();
-
-            // Division
-            Console.WriteLine("/ (Division): divides the value of numbers");
-            Console.WriteLine($"Example: {e} / {f} = {e / f}");
-            Console.WriteLine();
-
-            // Multiplication
-            Console.WriteLine("* (Multiplication): multiplies the value of numbers");
-            Console.WriteLine($"Example: {a} * {b} = {a * b}");
-            Console.WriteLine();
-
-            // Increment
-            Console.WriteLine("++ (Increment): adds the value of the number by 1");
-            Console.WriteLine($"Example: initial value = {c}, after ++ = {c++}, then final value = {c}");
-            Console.WriteLine();
-
-            // Decrement
-            Console.WriteLine("-- (Decrement): deducts the value of the number by 1");
-            Console.WriteLine($"Example: initial value = {d}, after -- = {d--}, then final value = {d}");
-            Console.WriteLine();
-
-            // Modulus
-            Console.WriteLine("% (Modulus): gives the remainder of the value of numbers");
-            Console.WriteLine($"Example: {a} % {b} = {a % b}");
-            Console.WriteLine();
-            Console.WriteLine("-----------------------------------------------------------------------------------");
+            Console.WriteLine($"Total Score: {player.Name} = {player.Score} | {computer.Name} = {computer.Score}");
             Console.WriteLine();
         }
+        private int PlayerTurn(int maxSides, out int chosenSides)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{player.Name} Your Turn!");
+            Console.WriteLine("Choose your die: d4, d6, d8, d12, d20");
+            string choice = Console.ReadLine();
 
+            int sides;
+
+            if (choice == "d4" && 4 <= maxSides) sides = 4;
+            else if (choice == "d6" && 6 <= maxSides) sides = 6;
+            else if (choice == "d8" && 8 <= maxSides) sides = 8;
+            else if (choice == "d12" && 12 <= maxSides) sides = 12;
+            else if (choice == "d20" && 20 <= maxSides) sides = 20;
+            else
+            {
+                Console.WriteLine($"Invalid input or number too high! You get a d{maxSides} by default.");
+                sides = maxSides;
+            }
+
+            chosenSides = sides; // Assigning out parameter
+
+            int roll = dice.Roll(sides);
+            Console.WriteLine($"You rolled a {roll} on a d{sides}!");
+            Console.WriteLine();
+            Console.WriteLine("- Press any key to continue -");
+            Console.ReadKey();
+            return roll;
+        }
+        private int ComputerTurn(int maxSides, out int chosenSides)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Opponent’s Turn!");
+
+            int[] diceOptions = { 4, 6, 8, 12, 20 };
+            int[] allowedDice = diceOptions.Where(d => d <= maxSides).ToArray();
+
+            int sides = allowedDice[rd.Next(allowedDice.Length)];
+
+            chosenSides = sides;
+
+            int roll = dice.Roll(sides);
+            Console.WriteLine($"Opponent chose d{sides} and rolled a {roll}!");
+            Console.WriteLine();
+            Console.WriteLine("- Press any key to continue -");
+            Console.ReadKey();
+            return roll;
+        }
         private void Outro()
         {
             // Goodbye message
